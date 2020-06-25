b4576503982019abf3941a66f25fdfae8f82c54a
hochungwong <chung940712@gmail.com>
2020-06-25 16:42:35 +1000

    shows dialog while swipe to delete item in cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -21,6 +21,30 @@ class CartItem extends StatelessWidget {
         padding: EdgeInsets.symmetric(vertical: 15, horizontal: 4),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (dismissDirection) {
+        return showDialog(
+            //showDialog return a Future
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text("Are you sure?"),
+                  content:
+                      Text("Do you want to remove the item from the cart?"),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("No"),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(false);
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("Yes"),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(true);
+                      },
+                    )
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItems(productId);
       },
